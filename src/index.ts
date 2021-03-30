49611f780e3eb8c704d71e9760d1a22f380b6ee6
이원희 <wonhee.lee@nhnsoft.com>
2021-03-30 14:55:45 +0900

    class로 blockchain 만들기

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const wonhee = new Human('wonhee', 24, 'female')
+const genesisBlock: Block = new Block(0, '20202020202020', '', 'Hello', 123456);
 
-const sayHiObject = (person: Human) => {
-    const {name, age, gender} = person;
-    console.log(`(Object) Hello! ${name}, you ar ${age}, you are a ${gender}`);
-}
+let blockchain: [Block] = [genesisBlock];
 
-sayHiObject(wonhee);
+console.log(blockchain);
 
 export {}
